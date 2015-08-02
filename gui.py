97fcb6016112a2db0105acae499cf3fc0dabc4f4
Kaushal <kaushal515@gmail.com>
2015-08-02 10:47:17 +0530

    gui user input bug fix

---
+++ gui.py
@@ -0,0 +1,49 @@
+from tkinter import *
+from tkinter.ttk import *
+import re
+import urllib.request
+import time
+
+def get_stock_price(*args):
+
+ symbol_in = str(symbol.get())
+
+ print(symbol_in)
+ i = 0  
+ while i < 3 :
+
+  i = i+1
+   
+  htmlfile = urllib.request.urlopen('http://finance.yahoo.com/q?s='+symbol_in+'&ql=0')
+
+  htmltext = htmlfile.read().decode()
+
+  regex = '<span id="yfs_l84_'+symbol_in+'">(.+?)</span>'
+
+  pattern = re.compile(regex)
+
+  price = re.findall(pattern,htmltext)
+
+  print (price)
+
+  time.sleep(5)
+
+root = Tk()
+root.title('Twitter Stream')
+
+#mainframe = tkinter.ttk.Frame(root)
+root.grid()
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+
+symbol = StringVar()
+
+symbol_entry = Entry(root, width=7, textvariable=symbol)
+symbol_entry.grid()
+
+symbol_label=Label(root, text="Stock Symbol").grid()
+
+stock_button=Button(root, text="Get stock price", command=get_stock_price).grid()
+
+root.mainloop()
+
